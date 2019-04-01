06fb9118a188b2c6ec74bd0905c4853897af3965
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2019-04-02 00:07:53 +0200

    image to icon in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,8 +5,8 @@
                 <i class="fas fa-bars p-3 text-white" onclick="sidebarToggle()"></i>
             </div>
             <div class="p-1 flex flex-row">
-                <img onclick="profileToggle()" class="inline-block h-8 w-8 rounded-full" src="https://avatars0.githubusercontent.com/u/4323180?s=460&v=4" alt="">
-                <a href="#" onclick="profileToggle()" class="text-white p-2 no-underline hidden md:block lg:block">Adam Wathan</a>
+                    <i onclick="profileToggle()" class="fas fa-user inline-block rounded-full py-2 text-white fill-current"></i>
+            <a href="#" onclick="profileToggle()" class="text-white p-2 no-underline hidden md:block lg:block">{{ auth()->user()->name}}</a>
                 <div id="ProfileDropDown" class="rounded hidden shadow-md bg-white absolute pin-t mt-12 mr-1 pin-r">
                     <ul class="list-reset">
                       <li><a href="#" class="no-underline px-4 py-2 block text-black hover:bg-grey-light">My account</a></li>
