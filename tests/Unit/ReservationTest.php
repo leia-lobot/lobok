5f41fc1acaa0930f845e171128bb3805dfb0ae53
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2019-04-01 20:53:18 +0200

    Name refactoring old bookings

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -5,25 +5,25 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class BookingTest extends TestCase
+class ReservationTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    public function a_booking_has_a_creator()
+    public function a_reservation_has_a_creator()
     {
         $user = factory('App\User')->create();
 
-        $booking = factory('App\Booking')->create(['user_id' => $user->id]);
+        $booking = factory('App\Reservation')->create(['user_id' => $user->id]);
 
         $this->assertEquals($user->id, $booking->user->id);
     }
 
     /** @test */
-    public function a_booking_requires_a_company()
+    public function a_reservation_requires_a_company()
     {
         $company = factory('App\Company')->create();
-        $booking = factory('App\Booking')->create(['company_id' => $company->id]);
+        $booking = factory('App\Reservation')->create(['company_id' => $company->id]);
 
         $this->assertEquals($company->id, $booking->company->id);
     }
@@ -36,10 +36,10 @@ class BookingTest extends TestCase
     */
 
     /** @test */
-    public function a_booking_requires_a_resource()
+    public function a_reservation_requires_a_resource()
     {
         $resource = factory('App\Resource')->create(['name' => 'Luminara']);
-        $booking = factory('App\Booking')->create(['resource_id' => $resource->id]);
+        $booking = factory('App\Reservation')->create(['resource_id' => $resource->id]);
 
         $this->assertEquals($resource->name, $booking->resource->name);
     }
