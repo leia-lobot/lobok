2dc182872b764dc3647cb3cdfe06b3277f08e02d
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-04-02 11:06:29 +0200

    Tests to check UserRoles when adding resource

--- tests/Feature/ResourcesTest.php
+++ tests/Feature/ResourcesTest.php
@@ -5,17 +5,19 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Role\UserRole;
 
 class ResourcesTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
-    
+
     /** @test */
     public function an_administrator_can_add_a_resource()
     {
         $this->withoutExceptionHandling();
         // Arrange
-        $user = factory('App\User')->create();
+        $user = factory('App\User')->create()->addRole(UserRole::ROLE_ADMIN);
+        $this->actingAs($user);
 
         $attributes = [
             'name' => $this->faker->sentence,
@@ -28,5 +30,43 @@ class ResourcesTest extends TestCase
         $this->assertDatabaseHas('resources', $attributes);
         $this->get('/resources')->assertSee($attributes['name']);
     }
-     
+
+    /** @test */
+    public function a_manager_can_add_a_resource()
+    {
+        $this->withoutExceptionHandling();
+        // Arrange
+        $user = factory('App\User')->create()->addRole(UserRole::ROLE_MANAGER);
+        $this->actingAs($user);
+
+        $attributes = [
+            'name' => $this->faker->sentence,
+        ];
+
+        // Act
+        $this->post('/resources', $attributes)->assertRedirect('/resources');
+
+        // Assert
+        $this->assertDatabaseHas('resources', $attributes);
+        $this->get('/resources')->assertSee($attributes['name']);
+    }
+
+    /** @test */
+    public function an_employer_can_not_add_a_resource()
+    {
+        //$this->withoutExceptionHandling();
+        // Arrange
+        $user = factory('App\User')->create()->addRole(UserRole::ROLE_EMPLOYER);
+        $this->actingAs($user);
+
+        $attributes = [
+            'name' => $this->faker->sentence,
+        ];
+
+        // Act
+        $this->post('/resources', $attributes)->assertForbidden();
+
+        // Assert
+        $this->assertDatabaseMissing('resources', $attributes);
+    }
 }
