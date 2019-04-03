b30a59126685c90f06594752e2e604d1e56b0ec3
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-04-03 14:00:25 +0200

    Added company as default to actAsUserWithRole

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,9 +8,14 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    public function actAsUserWithRole($role)
+    public function actAsUserWithRole($role, $extra = null)
     {
-        $user = factory('App\User')->create()->addRole($role);
+        if (is_null($extra)) {
+            $user = factory('App\User')->create(['company_id' => factory('App\Company')])->addRole($role);
+        } else {
+            $user = factory('App\User')->create($extra)->addRole($role);
+        }
+
         $this->actingAs($user);
     }
 }
