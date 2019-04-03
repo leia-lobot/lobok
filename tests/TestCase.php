d43fd4959b7cd7634b4b3cf834ca3ca1bfce8475
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-04-03 13:27:29 +0200

    Added actAsUserWithRole to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function actAsUserWithRole($role)
+    {
+        $user = factory('App\User')->create()->addRole($role);
+        $this->actingAs($user);
+    }
 }
