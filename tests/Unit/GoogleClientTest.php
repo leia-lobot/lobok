69a0b8efaeff27224e5dd87e4101e07a2c52b038
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-04-23 11:41:07 +0200

    Pulled out usable events for google calendar

--- tests/Unit/GoogleClientTest.php
+++ tests/Unit/GoogleClientTest.php
@@ -15,6 +15,36 @@ class GoogleClientTest extends TestCase
     {
         $gc = new Google();
         $service = $gc->service("Calendar");
-        dd($service);
+    }
+
+    private function createCalendar($service, $client)
+    {
+        $google_calendar = new \Google_Service_Calendar_Calendar($client);
+        $google_calendar->setSummary('Fiesta');
+        $google_calendar->setTimeZone('Europe/Stockholm');
+
+        $callback = $service->calendars->insert($google_calendar);
+        var_dump($callback);
+        return $callback;
+    }
+
+    private function listCalendars($service)
+    {
+        $calendars = $service->calendarList->listCalendarList();
+        var_dump($calendars);
+        return $calendars;
+    }
+
+    private function deleteCalendar($service, $id)
+    {
+        return $service->calendars->delete($id);
+    }
+
+    private function getCalendar($service, $id)
+    {
+        $calendar = $service->calendars->get($id);
+
+        var_dump($calendar);
+        return $calendar;
     }
 }
