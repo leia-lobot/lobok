4e7861fa21fd3830c95aa2f7238218c6124b1e34
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-05-02 12:02:59 +0200

    Added Roles trait

--- app/User.php
+++ app/User.php
@@ -4,10 +4,11 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Traits\HasRole;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasRole;
 
     /**
      * The attributes that are mass assignable.
@@ -34,59 +35,11 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'roles' => 'array',
+
     ];
 
     public function company()
     {
         return $this->belongsTo('App\Company');
-    }
-
-    public function addRole(string $role)
-    {
-        $roles = $this->getRoles();
-        $roles[] = $role;
-
-        $roles = array_unique($roles);
-        $this->setRoles($roles);
-
-        return $this;
-    }
-
-    public function setRoles(array $roles)
-    {
-        $this->setAttribute('roles', $roles);
-
-        return $this;
-    }
-
-    public function hasRole($roles)
-    {
-        $currentRoles = $this->getRoles();
-
-        if (is_array($roles)) {
-            foreach ($roles as $role) {
-                if (!in_array($role, $currentRoles)) {
-                    return false;
-                }
-            }
-        } else {
-            if (!in_array($roles, $currentRoles)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public function getRoles()
-    {
-        $roles = $this->getAttribute('roles');
-
-        if (is_null($roles)) {
-            $roles = [];
-        }
-
-        return $roles;
     }
 }
