506dc2df714d54d0d098906d2199f815ee4a66be
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-05-20 12:01:48 +0200

    Updated tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {
+      'regular': ['Gobold Regular'],
+      'lowplus': ['Gobold Lowplus'],
+    },
+  },
+  variants: {},
+  plugins: []
+}
