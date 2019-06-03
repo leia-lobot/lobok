3ef3e89f4538746d3504a0bacca694855e066902
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-06-03 12:04:42 +0200

    fixes to landing page, working on overview

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,43 @@
-@extends('layouts.blank')
+@extends('layouts.app')
 
 @section('content')
-    <div id="calendar">
+    <div class="flex flex-row h-screen">
+        <!-- Left -->
+        <div class="flex justify-center w-1/5 opacity-75 bg-red-900">
+            <h1 class="text-gray-100 text-6xl pt-16">LOBOK</h1>
+            
+        </div>
 
+        <!-- Right -->
+        <div class="w-4/5 bg-black opacity-75">
+            <div class="pt-40 flex justify-center">
+                    <div class="flex flex-col">
+                        <div class="flex justify-center">
+                                <a href="#reserve-modal" class="text-white rounded bg-red-700 px-5 py-2 shadow-2xl hover:bg-red-800 mr-10">Boka nu</a>
+                                <a href="#overview-modal" class="text-white rounded bg-red-700 px-5 py-2 shadow-2xl hover:bg-red-800">Översikt</a>
+                        </div>
+                    </div>
+                    
+                </div>
+            
+                @component('components.modal', ['name' => 'reserve-modal'])
+                <form action="">
+                        <input type="text" name="name" id="name" placeholder="Olle Johansson">
+                        <select name="company" id="company">
+                            <option value="leia">Leia Företagshotell</option>
+                        </select>
+                    <input type="date" name="start_date" id="start_date" value="{{ \Carbon\Carbon::now('Europe/Stockholm')->locale('sv')->format('Y-m-d') }}">
+                        <input type="date" name="end_date" id="end_date" value="{{ \Carbon\Carbon::now('Europe/Stockholm')->locale('sv')->format('Y-m-d') }}">
+                        <input type="time" name="start_time" id="start_time" value="{{ \Carbon\Carbon::now('Europe/Stockholm')->locale('sv')->set('minute',0)->format('H:i') }}">
+                        <input type="time" name="end_time" id="end_time" value="{{ \Carbon\Carbon::now('Europe/Stockholm')->locale('sv')->set('minute',0)->format('H:i') }}">
+                    </form>
+                @endcomponent
+                
+                @component('components.modal', ['name' => 'overview-modal'])
+                    <div id="calendar"></div>
+                @endcomponent
+        </div>
     </div>
+    
+
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
 <div class="bg-black w-screen h-screen opacity-75 fixed">
     
 </div>
-<div class="relative pt-10 flex justify-center">
+<div class="relative pt-40 flex justify-center">
     <div class="flex flex-col">
         <h2 class="text-red-700 text-3xl font-gobold">VÄLKOMMEN TILL</h2>
-        <h1 class="text-red-700 font-gobold" style="font-size: 200px">LOBOK</h1>
+        <h1 class="text-red-700 font-gobold -mt-10" style="font-size: 200px">LOBOK</h1>
         <div class="flex justify-center">
                 <a href="http://" class="text-white rounded bg-red-700 px-5 py-2 shadow-2xl hover:bg-red-800">Boka nu</a>
         </div>
