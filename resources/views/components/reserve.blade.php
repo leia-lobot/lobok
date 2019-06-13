3c2506fcc3b7841a49da0c10d4939121997799b6
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-06-13 12:09:12 +0200

    Fixes and added fields to form

--- resources/views/components/reserve.blade.php
+++ resources/views/components/reserve.blade.php
@@ -1,13 +1,9 @@
 @component('components.modal', ['name' => 'reserve-modal', 'style' => 'bg-red-900'])
 
-        <input type="time" name="start_time" id="start_time" value="{{ \Carbon\Carbon::now('Europe/Stockholm')->locale('sv')->set('minute',0)->format('H:i') }}">
-        <input type="time" name="end_time" id="end_time" value="{{ \Carbon\Carbon::now('Europe/Stockholm')->locale('sv')->set('minute',0)->format('H:i') }}">
-
-
 <form class="w-full max-w-lg">
 
   <div class="flex flex-wrap -mx-3 mb-3">    
-    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-name">
+    <label class="block tracking-wide text-gray-200 text-xs font-bold mb-2" for="grid-name">
         {{__('Namn')}}
     </label>
     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-name" type="text" placeholder="Jane">
@@ -17,8 +13,8 @@
   </div>
 
   <div class="flex flex-wrap -mx-3 mb-3">
-    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-company">
-      {{__('Företag')}}
+    <label class="block tracking-wide text-gray-200 text-xs font-bold mb-2" for="grid-company">
+    {{__('Företag')}}
     </label>
     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-company" type="text" placeholder="Leia Företagshotell">
     @error('grid-company')
@@ -27,7 +23,7 @@
   </div>
 
   <div class="flex flex-wrap -mx-3 mb-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-resource">
+      <label class="block tracking-wide text-gray-200 text-xs font-bold mb-2" for="grid-resource">
         {{__('Resurs')}}
       </label>
       <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-resource" type="text" placeholder="Leia Företagshotell">
@@ -37,18 +33,36 @@
     </div>
 
   <div class="flex flex-wrap -mx-3 mb-2">
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-start-date">
+    <div class="w-full md:w-1/2 pr-3 mb-6 md:mb-0">
+      <label class="block tracking-wide text-gray-200 text-xs font-bold mb-2" for="grid-start-date">
         {{ __('Från') }}
       </label>
       <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-start-date" type="date" placeholder="Albuquerque" value="{{ \Carbon\Carbon::now('Europe/Stockholm')->locale('sv')->format('Y-m-d') }}">
     </div>
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-end-date">
+    <div class="w-full md:w-1/2 pl-3 mb-6 md:mb-0">
+      <label class="block tracking-wide text-gray-200 text-xs font-bold mb-2" for="grid-end-date">
         {{ __('Till') }}
       </label>
       <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-end-date" type="date" placeholder="Albuquerque" value="{{ \Carbon\Carbon::now('Europe/Stockholm')->locale('sv')->format('Y-m-d') }}">
     </div>  
+  </div>
+  <div class="flex flex-wrap -mx-3 mb-2">
+    <div class="w-full md:w-1/2 pr-3 mb-6 md:mb-0">
+      <label class="block tracking-wide text-gray-200 text-xs font-bold mb-2" for="select-start">
+          {{ __('Tid') }}
+        </label>
+        <div id="timerange"></div>
+    </div>
+    <div class="w-full md:w-1/2 pl-3 mb-6 md:mb-0">
+      <label class="block tracking-wide text-gray-200 text-xs font-bold mb-2" for="grid-attendants">
+          {{ __('Deltagare') }}
+      </label>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-attendants" type="text" placeholder="Leia Företagshotell">
+      @error('grid-resource')
+      <p class="text-red-500 text-xs italic">Please fill out this field.</p> 
+      @enderror   
+    </div>
+      
   </div>
 </form>
 @endcomponent
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <h2 class="text-red-700 text-3xl font-gobold">VÄLKOMMEN TILL</h2>
         <h1 class="text-red-700 font-gobold -mt-10" style="font-size: 200px">LOBOK</h1>
         <div class="flex justify-center">
-                <a href="http://" class="text-white rounded bg-red-700 px-5 py-2 shadow-2xl hover:bg-red-800">Boka nu</a>
+        <a href="{{ url('/home')}}" class="text-white rounded bg-red-700 px-5 py-2 shadow-2xl hover:bg-red-800">Boka nu</a>
         </div>
     </div>
     
