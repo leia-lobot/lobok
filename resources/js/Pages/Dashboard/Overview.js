c0fb73b14c8442d72f64b818c5214c2047da30a7
Christoffer Isenberg <christoffer.isenberg@gmail.com>
2019-08-25 22:28:27 +0200

    FIX: Only show calendar when it has resources

--- resources/js/Pages/Dashboard/Overview.js
+++ resources/js/Pages/Dashboard/Overview.js
@@ -1,46 +1,45 @@
-import React from 'react';
-import { usePage } from '@inertiajs/inertia-react';
-import BigCalendar from 'react-big-calendar'
-import moment from 'moment'
+import React from "react";
+import { usePage } from "@inertiajs/inertia-react";
+import BigCalendar from "react-big-calendar";
+import moment from "moment";
 
-
-
-import Layout from '../../Shared/Layout';
+import Layout from "../../Shared/Layout";
 
 export default function() {
     const { events, resources } = usePage();
-    const localizer = BigCalendar.momentLocalizer(moment)
-    moment.locale('en-GB')
+    const localizer = BigCalendar.momentLocalizer(moment);
+    moment.locale("en-GB");
 
-    let parsedEvents = events.map( event => {
+    let parsedEvents = events.map(event => {
         return {
             id: event.id,
             title: event.title,
             start: moment(event.start).toDate(),
             end: moment(event.end).toDate(),
             resourceId: event.resourceId
-        }
-    })
+        };
+    });
 
-    console.log({parsedEvents});
-    
-    
     return (
         <Layout>
-            <BigCalendar
-                events={parsedEvents}
-                min={new Date(2017, 10, 0, 6, 0, 0)}
-                max={new Date(2017, 10, 0, 22, 0, 0)} 
-                localizer={localizer}
-                defaultView={BigCalendar.Views.DAY}
-                views={['day', 'work_week']}
-                step={30}
-                timeslots={2}
-                defaultDate={new Date()}
-                resources={resources}
-                resourceIdAccessor="resourceId"
-                resourceTitleAccessor="resourceTitle"
-            />
+            {resources.length > 0 ? (
+                <BigCalendar
+                    events={parsedEvents}
+                    min={new Date(2017, 10, 0, 6, 0, 0)}
+                    max={new Date(2017, 10, 0, 22, 0, 0)}
+                    localizer={localizer}
+                    defaultView={BigCalendar.Views.DAY}
+                    views={["day", "work_week"]}
+                    step={30}
+                    timeslots={2}
+                    defaultDate={new Date()}
+                    resources={resources}
+                    resourceIdAccessor="resourceId"
+                    resourceTitleAccessor="resourceTitle"
+                />
+            ) : (
+                <p>No Resources available</p>
+            )}
         </Layout>
-    )
+    );
 }
