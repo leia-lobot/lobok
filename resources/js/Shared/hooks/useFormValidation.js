f0a8ecb25f3d44a9e0a1b40c52c59bf45da57818
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-09-11 13:52:41 +0200

    ADD: Form validation

---
+++ resources/js/Shared/hooks/useFormValidation.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function useFormValidation(initialState, validate) {
+    const [values, setValues] = React.useState(initialState);
+    const [errors, setErrors] = React.useState({});
+    const [hasErrors, setHasErrors] = React.useState(false);
+    const [isSubmitting, setIsSubmitting] = React.useState(false);
+
+    React.useEffect(() => {
+        if (isSubmitting) {
+            const noErrors = Object.keys(errors).length === 0;
+            if (noErrors) {
+                console.log("authenticated!");
+                setIsSubmitting(false);
+                setHasErrors(false);
+            } else {
+                setIsSubmitting(false);
+                setHasErrors(true);
+            }
+        }
+    }, [errors, isSubmitting]);
+
+    function handleChange(event) {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    function handleBlur() {
+        const validationErrors = validate(values);
+        setErrors(validationErrors);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const validationErrors = validate(values);
+        setErrors(validationErrors);
+        setIsSubmitting(true);
+    }
+
+    return {
+        handleChange,
+        handleSubmit,
+        handleBlur,
+        values,
+        errors,
+        hasErrors,
+        isSubmitting
+    };
+}
+
+export default useFormValidation;
