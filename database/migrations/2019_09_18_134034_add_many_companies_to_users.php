167b734d6e7fefc9e691164de975fe295b956bbf
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-09-18 14:03:16 +0200

    ADD: users_companies migration

---
+++ database/migrations/2019_09_18_134034_add_many_companies_to_users.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddManyCompaniesToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_company_id_foreign');
+            $table->dropColumn('company_id');
+        });
+
+        Schema::create('companies_users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('company_id', false, true)->nullable();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->bigInteger('user_id', false, true)->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies_users');
+    }
+}
