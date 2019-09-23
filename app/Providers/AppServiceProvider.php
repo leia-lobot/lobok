919f009d943e249a7ec394f09bbef9ffab20ca55
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-09-23 12:03:34 +0200

    ADD: InertiaJS shared variables

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use PharIo\Manifest\Application;
 use Illuminate\Foundation\Application as IlluminateApplication;
 use Illuminate\Contracts\Foundation\Application as IlluminateContractsApplication;
+
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function register()
-    { }
+    {
+        $this->registerInertia();
+    }
 
     /**
      * Bootstrap any application services.
@@ -25,5 +31,36 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    public function registerInertia()
+    {
+        Inertia::version(function () {
+            return md5_file(public_path('mix-manifest.json'));
+        });
+
+        Inertia::share([
+            'auth' => function () {
+                return [
+                    'user' => Auth::user() ? [
+                        'id' => Auth::user()->id,
+                        'first_name' => Auth::user()->first_name,
+                        'last_name' => Auth::user()->last_name,
+                        'email' => Auth::user()->email,
+                        'role' => Auth::user()->role,
+                    ] : null,
+                ];
+            },
+            'flash' => function () {
+                return [
+                    'success' => Session::get('success'),
+                ];
+            },
+            'errors' => function () {
+                return Session::get('errors')
+                    ? Session::get('errors')->getBag('default')->getMessages()
+                    : (object) [];
+            },
+        ]);
     }
 }
