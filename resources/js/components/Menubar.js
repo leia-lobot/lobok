38ef95fadf7561fc500172fbbbee555db80d90b3
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-10-01 10:58:19 +0200

    ADD: authorization for menu

--- resources/js/components/Menubar.js
+++ resources/js/components/Menubar.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Menu } from "semantic-ui-react";
-import { InertiaLink } from "@inertiajs/inertia-react";
+import { InertiaLink, usePage } from "@inertiajs/inertia-react";
 
 function Menubar() {
     const pathname = window.location.pathname;
     const path = pathname === "/" ? "home" : pathname.substr(1);
     const [activeItem, setActiveItem] = useState(path);
+    const { auth } = usePage();
 
     const handleItemClick = e => setActiveItem(e.target.name);
 
@@ -31,28 +32,53 @@ function Menubar() {
             >
                 Overview
             </InertiaLink>
-            <Menu.Menu position="right">
-                <InertiaLink
-                    name="login"
-                    className={
-                        "item " + (activeItem === "login" ? "active" : "")
-                    }
-                    href={`/login`}
-                    onClick={handleItemClick}
-                >
-                    Login
-                </InertiaLink>
-                <InertiaLink
-                    name="register"
-                    className={
-                        "item " + (activeItem === "register" ? "active" : "")
-                    }
-                    href={`/register`}
-                    onClick={handleItemClick}
-                >
-                    Register
-                </InertiaLink>
-            </Menu.Menu>
+            {!auth.user ? (
+                <Menu.Menu position="right">
+                    <InertiaLink
+                        name="login"
+                        className={
+                            "item " + (activeItem === "login" ? "active" : "")
+                        }
+                        href={`/login`}
+                        onClick={handleItemClick}
+                    >
+                        Login
+                    </InertiaLink>
+
+                    <InertiaLink
+                        name="register"
+                        className={
+                            "item " +
+                            (activeItem === "register" ? "active" : "")
+                        }
+                        href={`/register`}
+                        onClick={handleItemClick}
+                    >
+                        Register
+                    </InertiaLink>
+                </Menu.Menu>
+            ) : (
+                <Menu.Menu position="right">
+                    <InertiaLink
+                        name="profile"
+                        className={
+                            "item " + (activeItem === "profile" ? "active" : "")
+                        }
+                        href={`/profile`}
+                        onClick={handleItemClick}
+                    >
+                        Profile
+                    </InertiaLink>
+                    <InertiaLink
+                        name="logout"
+                        className="item"
+                        href={`/logout`}
+                        method="post"
+                    >
+                        Logout
+                    </InertiaLink>
+                </Menu.Menu>
+            )}
         </Menu>
     );
 }
