46fed2fb1dca4cf4251c234318decf005ced8f4d
Christoffer Isenberg <yoda.christoffer@gmail.com>
2019-10-23 13:51:39 +0200

    FIX: Flashmesssage updates

--- resources/js/Shared/Layout.js
+++ resources/js/Shared/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "semantic-ui-react";
 
 import Menubar from "../components/Menubar";
-import FlashMessage from "./FlashMessage";
+import FlashMessage from "../components/FlashMessage";
 
 export default function Dashboard({ children }) {
     return (
